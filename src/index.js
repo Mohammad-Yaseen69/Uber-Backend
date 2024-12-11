034137debc7e1bbc12f88c9a0768762a2dfa49ab
Mohammad-Yaseen69 <yaseenyaseenali69@gmail.com>
2024-12-11 20:54:31 +0500

    Database Connected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import app from "./app.js";
+import { connectDB } from "./db/connectDB.js";
 import http from 'http'
 
 
 const server = http.createServer(app)
 const port = process.env.PORT || 4000
 
-server.listen(port , () => {
-    console.log(`Server is running on port ${port}`)
-})
+connectDB()
+    .then(() => {
+        server.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
